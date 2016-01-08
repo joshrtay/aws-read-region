cf1d76ec822f5193d52b6622391772b34cbb3882
Josh Taylor <joshrtay@gmail.com>
2016-01-08 11:27:21 -0800

    improve error handling

--- src/index.js
+++ src/index.js
@@ -26,8 +26,25 @@ function readRegion () {
   // grab region from ~/.aws/config
   let profile = env.AWS_PROFILE || 'default'
   let awsDir = path.join(env.HOME, '.aws')
-  let dotAWSConfig = ini.parse(fs.readFileSync(path.join(awsDir, 'config'), 'utf-8'))[profile]
+
+  try {
+    var configFile = fs.readFileSync(path.join(awsDir, 'config'), 'utf-8')
+  } catch (e) {
+    throw new Error("Can't read region, it has not been set in enviornment and ~/.aws/config does not exist.")
+  }
+
+  let dotAWSConfig = ini.parse(configFile)[profile]
+
+  if (!dotAWSConfig) {
+    throw new Error("Can't read region, no ~/.aws/config for profile: " + profile)
+  }
+
   config.region = dotAWSConfig.region
+
+  if (!config.region) {
+    throw new Error("Can't read region, no region specified in ~/.aws/config for profile: " + profile)
+  }
+
   return config
 }
 
